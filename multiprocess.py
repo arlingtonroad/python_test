474099d9534e93db2a8e1083a5f9b06e7ef3a428
xiaoshan <xiaoshan@example.com>
2015-07-02 15:37:02 +0800

    rm all python files

--- multiprocess.py
+++
@@ -1,8 +0,0 @@
-import os
-
-print 'Process (%s) start...' % os.getpid()
-pid = os.fork()
-if pid==0:
-	print 'I am child process (%s) and my parent is %s.'%(os.getpid(), os.getpid())
-else:
-	print 'I (%s) just created a child process (%s).' % (os.getpid(),pid)
